f74907398b2742ec58f8c910a7d92284876d4f74
Qiu RenBo <lampqiu@sina.com>
2020-08-23 21:25:07 +0800

    refactor(history): refactor style

--- src/app/shared/other-filter/other-filter.component.ts
+++ src/app/shared/other-filter/other-filter.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from "@angular/core";
 })
 export class OtherFilterComponent implements OnInit {
   constructor() {}
-
+  degrees = ["不限", "大专", "本科", "硕士"];
+  genders = ["不限", "男", "女"];
+  experiences = ["不限", "三支一扶", "社区干部"];
+  types = ["不限", "街道", "公安"];
   ngOnInit() {}
 }
